b9eb64c2a1cb27388e2ccd13f8e023e144d6942b
8connor <connorh16@gmail.com>
2020-12-03 14:39:06 -0500

    adding the calculation logic.

--- backEnd/definitions/definitions.js
+++ backEnd/definitions/definitions.js
@@ -34,11 +34,41 @@ module.exports = {
                 db.Prices.create({
                     Thickness: thicknessArr[b],
                     Pattern: nameArr[b]
-                }).then(mongObj => console.log(mongObj)).catch(err => console.log(err));
+                }).then(mongObj => console.log(mongObj))
+                    .catch(err => console.log(err));
             }
         });
     },
-    Calculate: (width, length, IG, pattern) => {
+    Calculate: (width, length, IG, pattern, thickness) => {
+        db.Prices.find({ Pattern: pattern, Thickness: thickness })
+            .lean()
+            .then(data => {
 
+                console.log(data);
+
+                let sqInch = width * length;
+                let sqFt = sqInch / 144;
+                let quantity = req.quantity;
+
+                if (quantity <= 5) {
+                    quantity = data.oneToFive
+                } else if (quantity >= 6 && quantity <= 10) {
+                    quantity = data.sixToTen
+                } else if (quantity >= 11 && quantity <= 25) {
+                    quantity = data.elevenToTwentyFive
+                }else if (quantity >= 26 && quantity <= 50){
+                    quantity = data.twentySixToFifty
+                }else if (quantity >= 51 && quantity <= 100){
+                    quantity = data.fiftyOneToOneHundred
+                }else if (quantity >= 101 && quantity <= 205){
+                    quantity = data.oneHundredOneToTwoHundredFive
+                }else if (quantity >= 251 && quantity <= 500){
+                    quantity = data.max
+                }
+
+                let cost = quantity * sqFt
+
+                return cost
+            })
     }
 };
